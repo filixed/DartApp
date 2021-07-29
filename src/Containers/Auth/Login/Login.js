d28a88c52d4145533b279305fa592778711f358e
filixed <filxed@gmail.com>
2021-07-29 12:40:08 +0200

    Added form to login component

--- src/Containers/Auth/Login/Login.js
+++ src/Containers/Auth/Login/Login.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import classes from './Login.module.css'
 
 const Login = () => {
     return (
-        <h1>Login Page</h1>
+        <div className={classes.Auth}>
+            <Form >
+                <Form.Group size="lg" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    autoFocus
+                    type="email"                    
+                />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"                  
+                />
+                </Form.Group>
+                <Button block size="lg" type="submit" >
+                Login
+                </Button>
+            </Form>
+        </div>
     )
 }
 
